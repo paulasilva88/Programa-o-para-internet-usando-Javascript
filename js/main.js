7f706467c48495b78e3c4cf61bb7a0e0acd977ce
Paula Silva <paulageovana0808@gmail.com>
2021-02-26 20:28:59 -0300

    Integrando o Html ao arquivo JS

---
+++ js/main.js
@@ -0,0 +1,17 @@
+/*var nome = "Paula"
+alert("Bem vinda, "+nome)
+var frase = "Meu nome é " + nome
+console.log(frase)
+console.log(frase.toLowerCase())
+
+*/
+
+var lista = [0, 1, 2]
+console.log(lista[0])
+console.log(lista.toString[0])
+console.log(lista.join(" , "))
+lista.push(5)
+console.log(lista)
+lista.pop()
+console.log(lista)
+console.log(lista.reverse())
